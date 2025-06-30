6542ed5c184d88be13e51ba21134ade49963544a
aquaseals <itzme.haya@gmail.com>
2025-06-30 06:33:51 -0400

    added recs, cleaned up, might be done??

--- home.js
+++ home.js
@@ -35,7 +35,7 @@ let authors = authorInfo.split(",")
 let authorSection = document.getElementsByClassName('author-list')[0]
 for (i=1; i<4; i++) {
     let author = document.createElement('h3')
-    author.style.color = "black !important"
+    author.class = "author-tag"
     let name = authors[i-1]
     if (i === 1) {
         name = name.slice(1)
@@ -62,7 +62,7 @@ genres[0] = genres[0].slice(1)
 let genreSection = document.getElementsByClassName('genre-list')[0]
 for (i=1; i<5; i++) {
     let genre = document.createElement('h3')
-    genre.style.color = "black !important"
+    genre.class = 'genre-tag'
     let g = genres[i-1]
     genre.innerHTML = `${i}. ${g}`
     genreSection.appendChild(genre)
@@ -74,7 +74,6 @@ recs[2] = recs[2].slice(0, -1)
 let recSection = document.getElementById('recommendation-cards')
 for (i=1; i<4; i++) {
     let rec = document.createElement('h3')
-    rec.style.color = "black !important"
     let name = rec[i-1]
     rec.innerHTML = `${i}. ${name}`
     recSection.appendChild(rec)
