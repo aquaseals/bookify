cb381bbc54483028b67e2761b7910cf3de2a5130
aquaseals <itzme.haya@gmail.com>
2025-06-30 04:36:58 -0400

    figuring out how to add all these value to html elements

--- home.js
+++ home.js
@@ -1,3 +1,10 @@
 let allData = localStorage.getItem("allData")
 
-console.log(allData)
+let profileInfo = allData[0]
+let bookInfo = allData[1]
+let authorInfo = allData[2]
+let genreInfo = allData[3]
+let fictionInfo = allData[4]
+let recommended = allData[5]
+
+console.log(profileInfo, bookInfo, authorInfo, genreInfo, fictionInfo, recommended)
