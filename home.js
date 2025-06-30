d5b2034dc6886fa85a8a4b8284a04e30c2114b30
aquaseals <itzme.haya@gmail.com>
2025-06-30 05:55:13 -0400

    fiction + non fiction percents added

--- home.js
+++ home.js
@@ -37,15 +37,21 @@ for (i=1; i<4; i++) {
     let author = document.createElement('h4')
     let name = authors[i-1]
     if (i === 1) {
-        name = name.slice(2)
+        name = name.slice(1)
     } 
     if (i === 2) {
-        name = name.slice(1, -1)
+        name = name.slice(0, 0)
     }
     if (i === 3) {
-        name = name.slice(0, -2)
+        name = name.slice(0, -1)
     }
     author.innerHTML = `${i}. ${name}`
     authorSection.appendChild(author)
 }
+
+let percents = fictionInfo.split(" ")
+let f = percents[0].slice(1, 0)
+let nf = percents[0].slice(0, -1)
+document.getElementById(`fiction-percent`).innerHTML = `${f}%`
+document.getElementById(`fiction-percent`).innerHTML = `${nf}%`
 
